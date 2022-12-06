f77ff2f57e816886091e37af1d54e29488306667
umutcansimsek75 <119808448+umutcansimsek75@users.noreply.github.com>
2022-12-06 20:38:28 +0300

    Nalan

---
+++ nalan/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'SCREANS/counter.dart';
+import 'SCREANS/nalan.dart';
+void main(List<String> args) {
+  runApp(const App());
+}
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Projem",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const Nalan(),
+    );
+  }
+  }
